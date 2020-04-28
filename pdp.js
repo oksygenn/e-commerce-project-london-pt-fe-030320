c8f61c5253e26d5161c867afe5ce80c168281831
Oksana Kostenko <kseniavlad31@gmail.com>
2020-04-28 20:15:49 +0100

    pdp adds to cart

--- pdp.js
+++ pdp.js
@@ -1,13 +1,11 @@
 const addToCartBtn = document.querySelector(".pdp-add-to-cart-button");
-// const detailsBtn = document.querySelector(".pdp-details-button");
 
 addToCartBtn.addEventListener("click", () => {
-  addToCartBtn.classList.add("green-button");
-  addToCartBtn.innerText = "Go to checkout";
+  if (!addToCartBtn.classList.contains("green-button")) {
+    addToCartBtn.classList.add("green-button");
+    addToCartBtn.innerText = "Go to checkout";
+    addToCart();
+  }
 });
-
-// detailsBtn.addEventListener("click", () => {
-//   detailsBtn;
-// });
 
 setupArrowButtons();
